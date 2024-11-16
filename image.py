5ab0df8c8a8ab965b39ad91e915f348779e6ac94
HARCHI_KUS <Sikret2345@gmail.com>
2024-11-16 12:19:06 +0500

    first

---
+++ image.py
@@ -0,0 +1,51 @@
+from PIL import Image
+import sys, os
+def image_to_ascii(name,format,end):
+    im = Image.open(f"./photos/{name}.{format}")
+    width,height = im.size
+
+    pix_value =list(im.getdata())
+    grid = []
+    for z in range(height):
+        grid.append(["X"] * width)
+    pix = im.load()
+
+
+    for y in range(height):
+        for x in range(width):
+            if sum(pix[x,y]) == 0:
+                grid[y][x] = "#"
+            elif sum(pix[x,y]) in range(1,100):
+                grid[y][x] = "X"
+            elif sum(pix[x,y]) in range(100,200):
+                grid[y][x] = "?"
+            elif sum(pix[x,y]) in range(200,300):
+                grid[y][x] = "&"
+            elif sum(pix[x,y]) in range(300,400):
+                grid[y][x] = "+"
+            elif sum(pix[x,y]) in range(400,500):
+                grid[y][x] = "/"
+            elif sum(pix[x,y]) in range(500,600):
+                grid[y][x] = "-"
+            elif sum(pix[x,y]) in range(600,700):
+                grid[y][x] = "*"
+            elif sum(pix[x,y]) in range(700,765):
+                grid[y][x] = "$"
+            else:
+                grid[y][x] = " "
+
+    file = open(f"{end}.txt","w")
+
+    for row in grid:
+        file.write("".join(row)+ "\n")
+
+    file.close()
+
+
+    print(sys.argv)
+
+name = input("Напишите название картинки которую вы хотите конвертировать в ascii: \n")
+format = input("Формат фото: \n")
+end = input("Под каким названием сохранить фото? ")
+
+image_to_ascii(name,format,end)
